ae1eb662ccd4358f5af665d145acb8859d342078
Claudio015 <c.cerullo@reply.it>
2015-07-28 11:18:20 +0200

    Aggiunto extension behavior per impostare username e password da configurazione

---
+++ Adapters/Behaviors/AdapterClientCredentialsExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reply.Cluster.Mercury.Adapters.Behaviors
+{
+    public class AdapterClientCredentialsExtension : BehaviorExtensionElement
+    {
+        [System.ComponentModel.Category("Credentials")]
+        [System.Configuration.ConfigurationProperty("UserName", DefaultValue = "anonymous")]
+        public string UserName
+        {
+            get
+            {
+                return ((string)(base["UserName"]));
+            }
+            set
+            {
+                base["UserName"] = value;
+            }
+        }
+
+        [System.ComponentModel.Category("Credentials")]
+        [System.Configuration.ConfigurationProperty("Password", DefaultValue = "")]
+        public string Password
+        {
+            get
+            {
+                return ((string)(base["Password"]));
+            }
+            set
+            {
+                base["Password"] = value;
+            }
+        }
+
+
+        protected override object CreateBehavior()
+        {
+            System.Diagnostics.Debugger.Launch();
+            var behavior = new AdapterClientCredentials();
+            behavior.UserName.UserName = this.UserName;
+            behavior.UserName.Password = this.Password;
+
+            return behavior;
+        }
+
+        public override Type BehaviorType
+        {
+            get { return typeof(AdapterClientCredentials); }
+        }
+    }
+}
